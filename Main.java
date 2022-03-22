b49cf48203850c06408bc0dc841b430927d40845
owokaiser <101186844+owokaiser@users.noreply.github.com>
2022-03-22 12:30:38 +0000

    final commit

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("liczby parzyste: ");
+    for(int x=0;x<50;x++){
+      if(x%2==0){
+      System.out.println(x);}
+      }
+    System.out.println("liczby parzyste: ");
+    for(int x=50;x<=100;x++){
+      if(x%2!=0){
+      System.out.println(x);}
+      }
   }
 }
